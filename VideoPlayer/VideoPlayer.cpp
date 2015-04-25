c31daa76e4b1f080aba4c306c22d260001b94e75
mokafolio <hi@mokafolio.de>
2015-04-25 22:18:14 +0000

    first working version of actual VideoPlayer cpp class

--- VideoPlayer/VideoPlayer.cpp
+++ VideoPlayer/VideoPlayer.cpp
@@ -32,6 +32,10 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <string.h>
 #include <iostream>
 #include <cmath>
+#include <thread>
+#include <atomic>
+#include <chrono>
+#include <cstdio>
 
 extern "C"
 {
@@ -46,6 +50,294 @@ extern "C"
   (a).nVersion.s.nVersionMinor = OMX_VERSION_MINOR; \
   (a).nVersion.s.nRevision = OMX_VERSION_REVISION; \
   (a).nVersion.s.nStep = OMX_VERSION_STEP
+
+class VideoPlayer
+{
+public:
+
+   typedef std::unique_lock<std::mutex> ScopedLock;
+
+   VideoPlayer(const std::string & _videoPath) :
+   m_omxClock(NULL),
+   m_omxVideoDecode(NULL),
+   m_omxVideoScheduler(NULL),
+   m_omxVideoRenderer(NULL),
+   m_ilClient(NULL),
+   m_videoFile(NULL)
+   {
+      m_ilClient = ilclient_init();
+      if(!m_ilClient)
+      {
+         m_errorString = "Could not initialize ILClient";
+         return;
+      }
+
+      if(OMX_Init() != OMX_ErrorNone)
+      {
+         m_errorString = "Could not initialize OMX";
+         return;
+      }
+
+      m_videoFile = std::fopen(_videoPath.c_str(), "rb");
+      if(!m_videoFile)
+      {
+         m_errorString = "Could not open video file at: " + _videoPath;
+         return;
+      }
+
+      memset(m_omxComponentList, 0, sizeof(m_omxComponentList));
+      memset(m_omxTunnels, 0, sizeof(m_omxTunnels));
+
+      // create video_decode
+      if(ilclient_create_component(m_ilClient, &m_omxVideoDecode, "video_decode", (ILCLIENT_CREATE_FLAGS_T)(ILCLIENT_DISABLE_ALL_PORTS | ILCLIENT_ENABLE_INPUT_BUFFERS)) != 0)
+      {
+         m_errorString = "Could not create omx video decorder.";
+         return;
+      }
+      m_omxComponentList[0] = m_omxVideoDecode;
+
+      // create video_render
+      if(ilclient_create_component(m_ilClient, &m_omxVideoRenderer, "video_render", ILCLIENT_DISABLE_ALL_PORTS) != 0)
+      {
+         m_errorString = "Could not create omx video renderer.";
+         return;
+      }
+      m_omxComponentList[1] = m_omxVideoRenderer;
+
+      // create clock
+      if(ilclient_create_component(m_ilClient, &m_omxClock, "clock", ILCLIENT_DISABLE_ALL_PORTS) != 0)
+      {
+         m_errorString = "Could not create omx clock.";
+         return;
+      }
+      m_omxComponentList[2] = m_omxClock;
+
+      // configure clock
+      OMX_TIME_CONFIG_CLOCKSTATETYPE cstate;
+      memset(&cstate, 0, sizeof(cstate));
+      cstate.nSize = sizeof(cstate);
+      cstate.nVersion.nVersion = OMX_VERSION;
+      cstate.eState = OMX_TIME_ClockStateWaitingForStartTime;
+      cstate.nWaitMask = 1;
+      if(OMX_SetParameter(ILC_GET_HANDLE(m_omxClock), OMX_IndexConfigTimeClockState, &cstate) != OMX_ErrorNone)
+      {
+         m_errorString = "Could not configure omx clock.";
+         return;
+      }
+
+      // create video_scheduler
+      if(ilclient_create_component(m_ilClient, &m_omxVideoScheduler, "video_scheduler", ILCLIENT_DISABLE_ALL_PORTS) != 0)
+      {
+         m_errorString = "Could not create omx video scheduler.";
+         return;
+      }
+      m_omxComponentList[3] = m_omxVideoScheduler;
+
+      set_tunnel(m_omxTunnels, m_omxVideoDecode, 131, m_omxVideoScheduler, 10);
+      set_tunnel(m_omxTunnels+1, m_omxVideoScheduler, 11, m_omxVideoRenderer, 90);
+      set_tunnel(m_omxTunnels+2, m_omxClock, 80, m_omxVideoScheduler, 12);
+
+      // setup clock tunnel first
+      if(ilclient_setup_tunnel(m_omxTunnels+2, 0, 0) != 0)
+      {
+         m_errorString = "Could not create omx clock tunnel.";
+         return;
+      }
+
+      ilclient_change_component_state(m_omxClock, OMX_StateExecuting);
+      ilclient_change_component_state(m_omxVideoDecode, OMX_StateIdle);
+
+      OMX_VIDEO_PARAM_PORTFORMATTYPE format;
+      memset(&format, 0, sizeof(OMX_VIDEO_PARAM_PORTFORMATTYPE));
+      format.nSize = sizeof(OMX_VIDEO_PARAM_PORTFORMATTYPE);
+      format.nVersion.nVersion = OMX_VERSION;
+      format.nPortIndex = 130;
+      format.eCompressionFormat = OMX_VIDEO_CodingAVC;
+
+      if(OMX_SetParameter(ILC_GET_HANDLE(m_omxVideoDecode), OMX_IndexParamVideoPortFormat, &format) != OMX_ErrorNone)
+      {
+         m_errorString = "Could not set video settings on omx decoder.";
+         return;
+      }
+
+      if(ilclient_enable_port_buffers(m_omxVideoDecode, 130, NULL, NULL, NULL) != 0)
+      {
+         m_errorString = "Could not enable port buffer on omx video decoder.";
+         return;
+      }
+
+      m_playbackThread = std::unique_ptr<std::thread>(new std::thread(std::bind(&VideoPlayer::playbackThreadFunc, this)));
+   }
+
+   ~VideoPlayer()
+   {
+      //join thread
+      if(m_playbackThread)
+      {
+         m_playbackThread->join();
+      }
+
+      //clean up
+   }
+
+   void pause()
+   {
+
+   }
+
+   void unpause()
+   {
+
+   }
+
+   void setSpeed(float _speed)
+   {
+
+   }
+
+   void seek(double _seconds)
+   {
+
+   }
+
+   double currentTime() const
+   {
+
+   }
+
+   double currentPosition() const
+   {
+
+   }
+
+   bool error() const
+   {
+      ScopedLock lock(m_mutex);
+      return m_errorString.length() > 0;
+   }
+
+   const std::string & errorMessage() const
+   {
+      ScopedLock lock(m_mutex);
+      return m_errorString;
+   }
+
+private:
+
+   void playbackThreadFunc()
+   {
+      /*
+      OMX_VIDEO_PARAM_PORTFORMATTYPE format;
+      OMX_TIME_CONFIG_CLOCKSTATETYPE cstate;
+      COMPONENT_T *video_decode = NULL, *video_scheduler = NULL, *video_render = NULL, *clock = NULL;
+      COMPONENT_T *list[5];
+      TUNNEL_T tunnel[4];
+      ILCLIENT_T *client;
+
+
+      FILE *in;
+      int status = 0;*/
+
+      unsigned int data_len = 0;
+      OMX_BUFFERHEADERTYPE *buf;
+      int port_settings_changed = 0;
+      int first_packet = 1;
+
+      {
+         ScopedLock lock(m_mutex);
+         ilclient_change_component_state(m_omxVideoDecode, OMX_StateExecuting);
+      }
+
+      do
+      {
+         ScopedLock lock(m_mutex);
+         
+         buf = ilclient_get_input_buffer(m_omxVideoDecode, 130, 1);
+
+         // feed data and wait until we get port settings changed
+         unsigned char * dest = buf->pBuffer;
+         data_len += std::fread(dest, 1, buf->nAllocLen-data_len, m_videoFile);
+
+         if(port_settings_changed == 0 &&
+            ((data_len > 0 && ilclient_remove_event(m_omxVideoDecode, OMX_EventPortSettingsChanged, 131, 0, 0, 1) == 0) ||
+             (data_len == 0 && ilclient_wait_for_event(m_omxVideoDecode, OMX_EventPortSettingsChanged, 131, 0, 0, 1,
+                                                       ILCLIENT_EVENT_ERROR | ILCLIENT_PARAMETER_CHANGED, 10000) == 0)))
+         {
+            port_settings_changed = 1;
+
+            if(ilclient_setup_tunnel(m_omxTunnels, 0, 0) != 0)
+            {
+               m_errorString = "could not set up omx tunnel.";
+               break;
+            }
+
+            ilclient_change_component_state(m_omxVideoScheduler, OMX_StateExecuting);
+
+            // now setup tunnel to video_render
+            if(ilclient_setup_tunnel(m_omxTunnels + 1, 0, 1000) != 0)
+            {
+               m_errorString = "could not set up omx render tunnel.";
+               break;
+            }
+
+            ilclient_change_component_state(m_omxVideoRenderer, OMX_StateExecuting);
+         }
+         
+         if(!data_len) fseek(m_videoFile, 0, SEEK_SET);
+
+         buf->nFilledLen = data_len;
+         data_len = 0;
+
+         buf->nOffset = 0;
+         if(first_packet)
+         {
+            buf->nFlags = OMX_BUFFERFLAG_STARTTIME;
+            first_packet = 0;
+         }
+         else
+            buf->nFlags = OMX_BUFFERFLAG_TIME_UNKNOWN;
+
+         if(OMX_EmptyThisBuffer(ILC_GET_HANDLE(m_omxVideoDecode), buf) != OMX_ErrorNone)
+         {
+            m_errorString = "Could not empty omx video decode buffer.";
+            break;
+         }
+
+      } while(buf);
+
+      buf->nFilledLen = 0;
+      buf->nFlags = OMX_BUFFERFLAG_TIME_UNKNOWN | OMX_BUFFERFLAG_EOS;
+
+      if(OMX_EmptyThisBuffer(ILC_GET_HANDLE(m_omxVideoDecode), buf) != OMX_ErrorNone)
+      {
+         m_errorString = "Could not empty omx video decode buffer.";
+      }
+
+      // wait for EOS from render
+      ilclient_wait_for_event(m_omxVideoRenderer, OMX_EventBufferFlag, 90, 0, OMX_BUFFERFLAG_EOS, 0,
+                              ILCLIENT_BUFFER_FLAG_EOS, 10000);
+
+      // need to flush the renderer to allow video_decode to disable its input port
+      ilclient_flush_tunnels(m_omxTunnels, 0);
+
+      ilclient_disable_port_buffers(m_omxVideoDecode, 130, NULL, NULL, NULL);
+   };
+
+   std::unique_ptr<std::thread> m_playbackThread;
+
+   mutable std::mutex m_mutex;
+
+   std::string m_errorString;
+
+   COMPONENT_T * m_omxClock;
+   COMPONENT_T * m_omxVideoDecode;
+   COMPONENT_T * m_omxVideoScheduler;
+   COMPONENT_T * m_omxVideoRenderer;
+   COMPONENT_T * m_omxComponentList[5];
+   TUNNEL_T m_omxTunnels[4];
+   ILCLIENT_T * m_ilClient;
+   std::FILE * m_videoFile;
+};
 
 static int video_decode_test(char *filename)
 {
@@ -97,15 +389,16 @@ static int video_decode_test(char *filename)
    OMX_TIME_CONFIG_SCALETYPE scaleType;
    OMX_INIT_STRUCTURE(scaleType);
 
-   float speed = 0.75;
+   float speed = 0.5;
    scaleType.xScale = std::floor((speed * std::pow(2,16)));
-    
+
    printf("set speed to : %.2f, %d\n", speed, scaleType.xScale);
     
    omx_err = OMX_SetParameter(ILC_GET_HANDLE(clock), OMX_IndexConfigTimeScale, &scaleType);
    if(omx_err != OMX_ErrorNone)
    {
         printf("Speed error setting OMX_IndexConfigTimeClockState : %d\n", omx_err);
+        return -1;
    }
 
    memset(&cstate, 0, sizeof(cstate));
@@ -243,7 +536,18 @@ int main (int argc, char **argv)
       exit(1);
    }
    bcm_host_init();
-   return video_decode_test(argv[1]);
+   //return video_decode_test(argv[1]);
+   VideoPlayer player(argv[1]);
+   if(player.error())
+   {
+      std::cout<<"Could not initialize video player: "<<player.errorMessage()<<std::endl;
+      return EXIT_FAILURE;
+   }
+   while(true)
+   {
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+   }
+   return EXIT_SUCCESS;
 }
 
 
